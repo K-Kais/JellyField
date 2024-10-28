211377679158193d82c513fb6ad63d33d025701b
Bình <kaisne0708@gmail.com>
2024-10-29 05:40:50 +0700

    Update Code

---
+++ Assets/Scripts/JellyCombineGrid.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+
+public class JellyCombineGrid
+{
+    private JellyCell[,] grid = new JellyCell[2, 4];
+    private List<JellyCell> results;
+
+    public void InitializeLeftRight(List<JellyCell> jellyCells)
+    {
+        grid[0, 0] = jellyCells[0]; grid[0, 2] = jellyCells[4];
+        grid[0, 1] = jellyCells[1]; grid[0, 3] = jellyCells[5];
+        grid[1, 0] = jellyCells[2]; grid[1, 2] = jellyCells[6];
+        grid[1, 1] = jellyCells[3]; grid[1, 3] = jellyCells[7];
+    }
+    public void InitializeTopDown(List<JellyCell> jellyCells)
+    {
+        grid[0, 0] = jellyCells[1]; grid[0, 2] = jellyCells[5];
+        grid[0, 1] = jellyCells[3]; grid[0, 3] = jellyCells[7];
+        grid[1, 0] = jellyCells[0]; grid[1, 2] = jellyCells[4];
+        grid[1, 1] = jellyCells[2]; grid[1, 3] = jellyCells[6];
+    }
+    public List<JellyCell> GetResults()
+    {
+        if (!NextTo()) return null;
+
+        results.AddRange(grid);
+        if (IsUniformColor(results)) return results;
+
+        var checks = new List<Func<bool>> { Square, LTopLeft, LTopRight, LDownLeft, LDownRight, HorizontalTop, HorizontalDown, NextTo };
+        foreach (var check in checks) if (check()) return results;
+        return null;
+    }
+    private bool IsUniformColor(List<JellyCell> cells) => cells.Select(cell => cell.jellyColor).Distinct().Count() == 1;
+    private bool Square()
+    {
+        results = new List<JellyCell>
+        {
+           grid[0, 0], grid[0, 1], grid[0, 2], grid[0, 3],
+           grid[1, 0], grid[1, 1]
+        };
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+           grid[0, 0], grid[0, 1], 
+           grid[1, 0], grid[1, 1], grid[1, 2], grid[1, 3],
+        };
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+           grid[0, 0],grid[0, 1], grid[0, 2], grid[0, 3],
+                                  grid[1, 2], grid[1, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+                                   grid[0, 2], grid[0, 3],
+            grid[1, 0],grid[1, 1], grid[1, 2], grid[1, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+           grid[0, 0], grid[0, 1], grid[0, 2],
+           grid[1, 0], grid[1, 1], grid[1, 2]
+        };
+        if (IsUniformColor(results)) return true;
+
+        var temp = results[5];
+        results.RemoveAt(5);
+        if (IsUniformColor(results)) return true;
+
+        results.Add(temp);
+        results.RemoveAt(2);
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+          grid[0, 1], grid[0, 2], grid[0, 3],
+          grid[1, 1], grid[1, 2], grid[1, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        temp = results[3];
+        results.RemoveAt(3);
+        if (IsUniformColor(results)) return true;
+
+        results.Add(temp);
+        results.RemoveAt(0);
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+            grid[0, 1], grid[0, 2],
+            grid[1, 1], grid[1, 2]
+        };
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool NextTo()
+    {
+        results = new List<JellyCell>
+        {
+            grid[0, 1], grid[0, 2],
+        };
+        if (IsUniformColor(results)) return true;
+
+        results = new List<JellyCell>
+        {
+            grid[1, 1], grid[1, 2]
+        };
+        if (IsUniformColor(results)) return true;
+
+        results.Clear();
+        return false;
+    }
+    private bool HorizontalTop()
+    {
+        results = new List<JellyCell>
+        {
+            grid[0, 0], grid[0, 1], grid[0, 2], grid[0, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        var temp = results[3];
+        results.RemoveAt(3);
+        if (IsUniformColor(results)) return true;
+
+        results.Add(temp);
+        results.RemoveAt(0);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool HorizontalDown()
+    {
+        results = new List<JellyCell>
+        {
+            grid[1, 0], grid[1, 1], grid[1, 2], grid[1, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        var temp = results[3];
+        results.RemoveAt(3);
+        if (IsUniformColor(results)) return true;
+
+        results.Add(temp);
+        results.RemoveAt(0);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool LTopLeft()
+    {
+        results = new List<JellyCell>
+        {
+            grid[0, 1], grid[0, 2], grid[0, 3],
+            grid[1, 1],
+        };
+        if (IsUniformColor(results)) return true;
+
+        results.RemoveAt(2);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool LTopRight()
+    {
+        results = new List<JellyCell>
+        {
+            grid[0, 0],grid[0, 1], grid[0, 2],
+                                   grid[1, 2],
+        };
+        if (IsUniformColor(results)) return true;
+
+        results.RemoveAt(0);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool LDownLeft()
+    {
+        results = new List<JellyCell>
+        {
+            grid[0, 1],
+            grid[1, 1], grid[1, 2], grid[1, 3]
+        };
+        if (IsUniformColor(results)) return true;
+
+        results.RemoveAt(3);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+    private bool LDownRight()
+    {
+        results = new List<JellyCell>
+        {
+                                   grid[0, 2],
+            grid[1, 0],grid[1, 1], grid[1, 2],
+        };
+        if (IsUniformColor(results)) return true;
+
+        results.RemoveAt(1);
+        if (IsUniformColor(results)) return true;
+        return false;
+    }
+}
--- Assets/Scripts/Jellyfier.cs
+++ Assets/Scripts/Jellyfier.cs
@@ -1,5 +1,6 @@
 ﻿using Sirenix.OdinInspector;
 using Sirenix.Serialization;
+using Sirenix.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -938,8 +939,9 @@ public class Jellyfier : SerializedMonoBehaviour
     public void Combine()
     {
         SetGridCell();
-        var jellyToCombine = new Dictionary<GridCell, JellyCell>();
+        var jellyToCombine = new List<JellyCell>();
         var jellyGrid = new List<JellyCell>();
+        JellyCombineGrid jellyCombineGrid = new JellyCombineGrid();
 
         foreach (var keyValuePair in gridCell.neighbors)
         {
@@ -953,77 +955,36 @@ public class Jellyfier : SerializedMonoBehaviour
                 jellyGrid = new List<JellyCell>();
                 jellyGrid.AddRange(jellyfierNeighbor.jellyCellDic.Values);
                 jellyGrid.AddRange(jellyCellDic.Values);
-                JellyCombineGrid jellyCombineGrid = new JellyCombineGrid();
                 jellyCombineGrid.InitializeLeftRight(jellyGrid);
-
-                var leftResults = jellyCombineGrid.GetResults();
-                if (leftResults != null)
-                {
-                    foreach (var jelly in leftResults)
-                    {
-                        Debug.Log(jelly.jellyColor);
-                    }
-                }
-                continue;
+                jellyToCombine.AddRange(jellyCombineGrid.GetResults());
             }
             else if (direction == GridDirection.Right)
             {
                 jellyGrid = new List<JellyCell>();
                 jellyGrid.AddRange(jellyCellDic.Values);
                 jellyGrid.AddRange(jellyfierNeighbor.jellyCellDic.Values);
-                JellyCombineGrid jellyCombineGrid = new JellyCombineGrid();
                 jellyCombineGrid.InitializeLeftRight(jellyGrid);
 
-                var leftResults = jellyCombineGrid.GetResults();
-                if (leftResults != null)
-                {
-                    foreach (var jelly in leftResults)
-                    {
-                        Debug.Log(jelly.jellyColor);
-                    }
-                }
-                continue;
+                jellyToCombine.AddRange(jellyCombineGrid.GetResults());
             }
             else if (direction == GridDirection.Top)
             {
                 jellyGrid = new List<JellyCell>();
                 jellyGrid.AddRange(jellyfierNeighbor.jellyCellDic.Values);
                 jellyGrid.AddRange(jellyCellDic.Values);
-                JellyCombineGrid jellyCombineGrid = new JellyCombineGrid();
                 jellyCombineGrid.InitializeTopDown(jellyGrid);
 
                 var leftResults = jellyCombineGrid.GetResults();
-                if (leftResults != null)
-                {
-                    foreach (var jelly in leftResults)
-                    {
-                        Debug.Log(jelly.jellyColor);
-                    }
-                }
-                continue;
             }
             else if (direction == GridDirection.Down)
             {
                 jellyGrid = new List<JellyCell>();
                 jellyGrid.AddRange(jellyCellDic.Values);
                 jellyGrid.AddRange(jellyfierNeighbor.jellyCellDic.Values);
-                JellyCombineGrid jellyCombineGrid = new JellyCombineGrid();
                 jellyCombineGrid.InitializeTopDown(jellyGrid);
-
-                var leftResults = jellyCombineGrid.GetResults();
-                if (leftResults != null)
-                {
-                    foreach (var jelly in leftResults)
-                    {
-                        Debug.Log(jelly.jellyColor);
-                    }
-                }
-                continue;
+                jellyToCombine.AddRange(jellyCombineGrid.GetResults());
             }
         }
-
-
-
 
         //foreach (var keyValuePair in gridCell.neighbors)
         //{
@@ -1101,210 +1062,4 @@ public enum MeshType
     DownLeftRight,
     LeftTopDown,
     RightTopDown,
-}
-
-public class JellyCombineGrid
-{
-    private JellyCell[,] grid = new JellyCell[2, 4];
-    private List<JellyCell> results;
-
-    public void InitializeLeftRight(List<JellyCell> jellyCells)
-    {
-        grid[0, 0] = jellyCells[0]; grid[0, 2] = jellyCells[4];
-        grid[0, 1] = jellyCells[1]; grid[0, 3] = jellyCells[5];
-        grid[1, 0] = jellyCells[2]; grid[1, 2] = jellyCells[6];
-        grid[1, 1] = jellyCells[3]; grid[1, 3] = jellyCells[7];
-    }
-    public void InitializeTopDown(List<JellyCell> jellyCells)
-    {
-        grid[0, 0] = jellyCells[1]; grid[0, 2] = jellyCells[5];
-        grid[0, 1] = jellyCells[3]; grid[0, 3] = jellyCells[7];
-        grid[1, 0] = jellyCells[0]; grid[1, 2] = jellyCells[4];
-        grid[1, 1] = jellyCells[2]; grid[1, 3] = jellyCells[6];
-    }
-    public List<JellyCell> GetResults()
-    {
-        if (!NextTo()) return null;
-
-        results.AddRange(grid);
-        if (IsUniformColor(results)) return results;
-
-        var checks = new List<Func<bool>> { Square, LTopLeft, LTopRight, LDownLeft, LDownRight, HorizontalTop, HorizontalDown, NextTo };
-        foreach (var check in checks) if (check()) return results;
-        return null;
-    }
-    private bool IsUniformColor(List<JellyCell> cells) => cells.Select(cell => cell.jellyColor).Distinct().Count() == 1;
-    private bool Square()
-    {
-        results = new List<JellyCell>
-        {
-           grid[0, 0], grid[0, 1], grid[0, 2], grid[0, 3],
-           grid[1, 0], grid[1, 1]
-        };
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-           grid[0, 0], grid[0, 1], 
-           grid[1, 0], grid[1, 1], grid[1, 2], grid[1, 3],
-        };
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-           grid[0, 0],grid[0, 1], grid[0, 2], grid[0, 3],
-                                  grid[1, 2], grid[1, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-                                   grid[0, 2], grid[0, 3],
-            grid[1, 0],grid[1, 1], grid[1, 2], grid[1, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-           grid[0, 0], grid[0, 1], grid[0, 2],
-           grid[1, 0], grid[1, 1], grid[1, 2]
-        };
-        if (IsUniformColor(results)) return true;
-
-        var temp = results[5];
-        results.RemoveAt(5);
-        if (IsUniformColor(results)) return true;
-
-        results.Add(temp);
-        results.RemoveAt(2);
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-          grid[0, 1], grid[0, 2], grid[0, 3],
-          grid[1, 1], grid[1, 2], grid[1, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        temp = results[3];
-        results.RemoveAt(3);
-        if (IsUniformColor(results)) return true;
-
-        results.Add(temp);
-        results.RemoveAt(0);
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-            grid[0, 1], grid[0, 2],
-            grid[1, 1], grid[1, 2]
-        };
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-
-    private bool NextTo()
-    {
-        results = new List<JellyCell>
-        {
-            grid[0, 1], grid[0, 2],
-        };
-        if (IsUniformColor(results)) return true;
-
-        results = new List<JellyCell>
-        {
-            grid[1, 1], grid[1, 2]
-        };
-        if (IsUniformColor(results)) return true;
-
-        results.Clear();
-        return false;
-    }
-
-    private bool HorizontalTop()
-    {
-        results = new List<JellyCell>
-        {
-            grid[0, 0], grid[0, 1], grid[0, 2], grid[0, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        var temp = results[3];
-        results.RemoveAt(3);
-        if (IsUniformColor(results)) return true;
-
-        results.Add(temp);
-        results.RemoveAt(0);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-    private bool HorizontalDown()
-    {
-        results = new List<JellyCell>
-        {
-            grid[1, 0], grid[1, 1], grid[1, 2], grid[1, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        var temp = results[3];
-        results.RemoveAt(3);
-        if (IsUniformColor(results)) return true;
-
-        results.Add(temp);
-        results.RemoveAt(0);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-    private bool LTopLeft()
-    {
-        results = new List<JellyCell>
-        {
-            grid[0, 1], grid[0, 2], grid[0, 3],
-            grid[1, 1],
-        };
-        if (IsUniformColor(results)) return true;
-
-        results.RemoveAt(2);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-    private bool LTopRight()
-    {
-        results = new List<JellyCell>
-        {
-            grid[0, 0],grid[0, 1], grid[0, 2],
-                                   grid[1, 2],
-        };
-        if (IsUniformColor(results)) return true;
-
-        results.RemoveAt(0);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-    private bool LDownLeft()
-    {
-        results = new List<JellyCell>
-        {
-            grid[0, 1],
-            grid[1, 1], grid[1, 2], grid[1, 3]
-        };
-        if (IsUniformColor(results)) return true;
-
-        results.RemoveAt(3);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
-    private bool LDownRight()
-    {
-        results = new List<JellyCell>
-        {
-                                   grid[0, 2],
-            grid[1, 0],grid[1, 1], grid[1, 2],
-        };
-        if (IsUniformColor(results)) return true;
-
-        results.RemoveAt(1);
-        if (IsUniformColor(results)) return true;
-        return false;
-    }
 }
